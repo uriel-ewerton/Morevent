5b9c5d0175756ef2a50eec2f33daec04fe04c2b0
uriel-ewerton <uriel.ewerton1@gmail.com>
2023-12-08 23:04:15 -0300

    módulos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
+import { toast, ToastConteiner } from "react-toastify"
 import RoutesApp from "./routes";
 import { AuthProvider } from "./contexts/auth";
 
 const App = () => {
   return (
     <AuthProvider>
-    
+      <ToastConteiner autoClose={3000} position={toast.POSITION.BOTTOM_CENTER} />
       <RoutesApp />
       <GlobalStyle />
       
