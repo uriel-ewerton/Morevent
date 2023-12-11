f4f14aae580902c5ac3f0e211494210b22ebbfcf
uriel-ewerton <uriel.ewerton1@gmail.com>
2023-12-11 02:39:31 -0300

    limpeza

--- frontend/src/pages/HomeLogado/index.js
+++ frontend/src/pages/HomeLogado/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import HeaderPopup from "../../components/pop-ups/HeaderPopup";
-import Button from "../../components/pop-ups/Button";
 import useAuth from "../../hooks/useAuth";
 
 const HomeLogado = () => {
